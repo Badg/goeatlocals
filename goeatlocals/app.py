47171498da32622fea06342d8d20075dc8f097b6
Nick Badger <nbadger1@gmail.com>
2020-06-27 21:57:25 -0700

    Add hard-coded get all places API

--- goeatlocals/app.py
+++ goeatlocals/app.py
@@ -3,7 +3,10 @@ import functools
 import trio
 from hypercorn.config import Config
 from hypercorn.trio import serve
+from quart import jsonify
 from quart_trio import QuartTrio
+
+from goeatlocals.places import PLACES
 
 app = QuartTrio(__name__)
 
@@ -11,6 +14,11 @@ app = QuartTrio(__name__)
 @app.route('/api/')
 async def index():
     return b'Hello. World?'
+
+
+@app.route('/api/places')
+async def get_all_places():
+    return jsonify(PLACES)
 
 
 def run_app():
---
+++ goeatlocals/places.py
@@ -0,0 +1,99 @@
+'''Temporarily hard-code places just so we can get things going on the
+frontend.
+'''
+
+PLACES = [
+    {
+        'name': 'Drexl',
+        'placeID': 1,
+        'placeLong': -122.2674272,
+        'placeLat': 37.8072493,
+        'placeDetails': {
+            'osmID': 3056134586,
+            'hasFood': False,
+            'hasBooze': True,
+            'hasBeer': True,
+            'hasProvisions': False
+        }
+    },
+    {
+        'name': '355',
+        'placeID': 2,
+        'placeLong': -122.2669739,
+        'placeLat': 37.8068275,
+        'placeDetails': {
+            'osmID': 1079401457,
+            'hasFood': False,
+            'hasBooze': True,
+            'hasBeer': True,
+            'hasProvisions': False
+        }
+    },
+    {
+        'name': 'Itani Ramen',
+        'placeID': 3,
+        'placeLong': -122.2699126,
+        'placeLat': 37.8076042,
+        'placeDetails': {
+            'osmID': 4095991379,
+            'hasFood': True,
+            'hasBooze': False,
+            'hasBeer': True,
+            'hasProvisions': False
+        }
+    },
+    {
+        'name': 'The Telegraph',
+        'placeID': 4,
+        'placeLong': -122.2685736,
+        'placeLat': 37.8128233,
+        'placeDetails': {
+            'osmID': 3470332401,
+            'hasFood': True,
+            'hasBooze': True,
+            'hasBeer': True,
+            'hasProvisions': False
+        }
+    },
+    {
+        'name': 'Duende',
+        'placeID': 5,
+        'placeLong': -122.2695932,
+        'placeLat': 37.8081307,
+        'placeDetails': {
+            'osmID': 3470332399,
+            'hasFood': True,
+            'hasBooze': True,
+            'hasBeer': True,
+            'hasProvisions': False
+        }
+    },
+    {
+        'name': 'The Lunch Box',
+        'placeID': 6,
+        'placeLong': -122.2680771,
+        'placeLat': 37.8065924,
+        'placeDetails': {
+            'osmID': 1186413873,
+            'hasFood': True,
+            'hasBooze': False,
+            # Note I don't actually know this for sure; I've never been
+            # there
+            'hasBeer': False,
+            'hasProvisions': False
+        }
+    },
+    {
+        'name': 'Howden Market',
+        'placeID': 7,
+        'placeLong': -122.2672438,
+        'placeLat': 37.8055483,
+        'placeDetails': {
+            'osmID': 3927998690,
+            'hasFood': True,
+            'hasBooze': False,
+            'hasBeer': True,
+            'hasProvisions': True
+        }
+    }
+]
