5b58eb146f93fe83617e4e177a52d7f9e84fad30
Nick Badger <nbadger1@gmail.com>
2020-08-08 15:08:35 -0700

    Add: use TTL cache for city:places data

--- goeatlocals/cities.py
+++ goeatlocals/cities.py
@@ -1,6 +1,9 @@
 '''Temporary hard-coded bounding boxes for cities.
 '''
+import cachetools
 
+# Cache city data for a max of 5 minutes
+CACHE_TTL_SECONDS = 300
 CITIES = {
     'oakland': {
         'left': -122.355881,
@@ -9,5 +12,4 @@ CITIES = {
         'top': 37.885368
     }
 }
-
-CITY_CACHES = {}
+CITY_CACHES = cachetools.TTLCache(maxsize=100, ttl=CACHE_TTL_SECONDS)
